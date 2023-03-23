4de2a7f3afab7e4dc5b41983d33be1479de6ad25
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-03-23 15:16:32 +0530

    initial commit

---
+++ react-docs.js
@@ -0,0 +1,7 @@
+// React app id made up of components->piece of UI having own logic and implementation.
+// React component is JS function that returns Promise.
+
+//Rules:
+//1. Component name starts with capital letter
+//2. It shuould be closed with / Eg: <MyButton />
+
