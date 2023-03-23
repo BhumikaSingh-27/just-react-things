bb43e2f6814def24eae1d666667268708af1d97e
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-03-24 02:01:20 +0530

    initial commit

--- react-docs.js
+++ react-docs.js
@@ -5,3 +5,62 @@
 //1. Component name starts with capital letter
 //2. It shuould be closed with / Eg: <MyButton />
 
+// Ques. Why do we wrap JSX code in div or empty fragmanet?
+// Component coon't be run with multiple JSX Tages.
+
+// We use className attribute to style element as per class, same lie we use class in HTML
+
+
+//JSX helps us to put Mark UP into HTML
+
+//Displaying data
+// user- object, data can be displayed using . operator included in curly braces (JSX: since we are writing HTML)
+// {user.name}
+
+//Conditional Rendering
+// based on the condition we apply rendering
+// let islogin = true
+// if(islogin){
+//     <admin />
+// }else{
+//     <admin />
+// }
+//We can also store component invocation in a variable and then render it as a js variable:
+//Eg: 
+// let content;
+// if (isLoggedIn) {
+//   content = <AdminPanel />;
+// } else {
+//   content = <LoginForm />;
+// }
+// return (
+//   <div>
+//     {content}
+//   </div>
+// );
+
+//using ternary operator
+/* <div>
+  {isLoggedIn ? (
+    <AdminPanel />
+  ) : (
+    <LoginForm />
+  )}
+</div> */
+
+//we don't else
+/* <div>
+  {isLoggedIn && <AdminPanel />}
+</div> */
+
+//Rendering lists
+const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
+
+  const listItem = product.map(item=> <li>{item.title}</li>)
+  return (<ul>{listItem}</ul>)
+
